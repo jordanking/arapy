f4f210309ec4df030b8f90f9dcb96f9a92487ce6
jordanking <king96@lakeclark.llnl.gov>
2015-07-10 11:17:53 -0700

    First commit, initializing

---
+++ arapy.py
@@ -0,0 +1,7 @@
+### Arapy module!
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import re as re
+import codecs
---
+++ preprocessing.py
@@ -0,0 +1,5 @@
+### Preprocessing tools for arabic
+
+from __future__ import absolute_import
+from __future__ import print_function
+
